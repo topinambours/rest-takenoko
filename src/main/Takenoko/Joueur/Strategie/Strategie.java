67164320d62ec025dc5857529d3daa85fea8a0b5
Fontany--Legall Brandon <brandon.fontany--legall@etu.unice.fr>
2018-09-20 14:14:07 +0200

    Strategie #7

---
+++ src/main/Takenoko/Joueur/Strategie/Strategie.java
@@ -0,0 +1,7 @@
+package Takenoko.Joueur.Strategie;
+
+import Takenoko.Parcel.CoordAxial;
+
+public interface Strategie {
+    public CoordAxial getCoord();
+}
---
+++ src/main/Takenoko/Joueur/Strategie/StrategieRandom.java
@@ -0,0 +1,13 @@
+package Takenoko.Joueur.Strategie;
+
+import Takenoko.Parcel.CoordAxial;
+
+import java.util.Random;
+
+public class StrategieRandom implements Strategie{
+
+    public CoordAxial getCoord() {
+        Random random = new Random();
+        return new CoordAxial(random.nextInt(),random.nextInt());
+    }
+}
