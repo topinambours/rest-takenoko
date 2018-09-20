99b23561dfe0c40ee756d1b956a9c7fb2b65f72d
THOMAS MAHE <mt405333@etu.unice.fr>
2018-09-20 11:26:38 +0200

    Ajout de la classe Parcel
    #1

---
+++ src/main/Takenoko/Parcel.java
@@ -0,0 +1,35 @@
+package Takenoko;
+
+import java.util.Vector;
+
+public class Parcel {
+
+    /**
+     * coord must be a class
+     */
+    private Vector<Integer> coord;
+
+    public Parcel(int q, int r){
+        this.coord = new Vector<>();
+        this.coord.add(0,q);
+        this.coord.add(1,r);
+    }
+
+    public Parcel(){
+        this(0,0);
+    }
+
+    public int getq(){
+        return coord.get(0);
+    }
+
+    public int getr(){
+        return coord.get(1);
+    }
+
+    public void setCoord(int q, int r){
+        this.coord.set(0,q);
+        this.coord.set(1,r);
+    }
+
+}
---
+++ src/test/Takenoko/ParcelTest.java
@@ -0,0 +1,31 @@
+package Takenoko;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ParcelTest {
+
+    @Test
+    public void getq() {
+        Parcel p = new Parcel(10,15);
+        assertEquals(10, p.getq());
+    }
+
+    @Test
+    public void getr() {
+        Parcel p = new Parcel(10,15);
+        assertEquals(15, p.getr());
+    }
+
+    @Test
+    public void setCoord() {
+        Parcel p = new Parcel(10,15);
+        assertEquals(10, p.getq());
+        assertEquals(15, p.getr());
+
+        p.setCoord(15,10);
+        assertEquals(15, p.getq());
+        assertEquals(10, p.getr());
+    }
+}
