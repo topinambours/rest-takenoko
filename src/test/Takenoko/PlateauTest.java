e3e0a7bd318834f15a790a3e6e23709dbbb16087
Matthias Percelay <matthias.percelay1@gmail.com>
2018-09-20 15:17:02 +0200

    maj tests unitaires pour Plateau
    #4

--- src/test/Takenoko/PlateauTest.java
+++ src/test/Takenoko/PlateauTest.java
@@ -1,5 +1,7 @@
 package Takenoko;
 
+import Takenoko.Joueur.Joueur;
+import Takenoko.Joueur.Strategie.StrategieRandom;
 import Takenoko.Parcel.CoordAxial;
 import Takenoko.Parcel.Parcel;
 import Takenoko.Plateau;
@@ -16,5 +18,15 @@ public class PlateauTest {
         var coo = new CoordAxial(3, 5);
         plat.putParcel(parc, coo);
         assertEquals(parc, plat.getParcel(3, 5));
+    }
+
+    @Test
+    public void newputParcel() {
+        var plat = new Plateau();
+        var parc = new Parcel();
+        var bot = new Joueur(1, new StrategieRandom());
+        var coo = bot.putParcel(parc, plat);
+        assertEquals(coo.getQ(), parc.getq());
+        assertEquals(coo.getR(), parc.getr());
     }
 }
