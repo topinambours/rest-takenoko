b5c0929954c47680bb22362337e4eca0c272a35d
Fontany--Legall Brandon <brandon.fontany--legall@etu.unice.fr>
2018-09-24 15:09:26 +0200

    delete couleur

--- src/main/Takenoko/Plot/Plot.java
+++ src/main/Takenoko/Plot/Plot.java
@@ -3,25 +3,15 @@ package Takenoko.Plot;
 public class Plot {
 
     private CoordAxial coord;
-    private Couleur couleur;
 
-    public Plot(int q, int r,Couleur couleur){
+    public Plot(int q, int r){
         this.coord = new CoordAxial(q,r);
-        //this.couleur = couleur;
-    }
-/*
-    public Plot(Couleur couleur){
-        this(0,0,couleur);
-    }
-
-    public Plot(int q,int r){
-        this(q,r,Couleur.ROUGE);
     }
 
     public Plot(){
-        this(0,0,Couleur.ROUGE);
+        this(0,0);
     }
-    */
+
 
     public int getq(){
         return coord.getQ();
@@ -40,15 +30,6 @@ public class Plot {
         coord.setR(r);
     }
 
-    /*
-    public Couleur getCouleur() {
-        return couleur;
-    }
-
-    public void setCouleur(Couleur couleur) {
-        this.couleur = couleur;
-    }
-    */
 
     @Override
     public String toString(){
