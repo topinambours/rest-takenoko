5ca025d2c72cebddff38cde802f545b2853918b5
Matthias Percelay <matthias.percelay1@gmail.com>
2018-10-01 13:45:13 +0200

    Revert "Merge remote-tracking branch 'origin/release5' into release5"
    This reverts commit c0784f840c7fd441960f71edf04687ffacbff230.

--- src/test/Takenoko/Util/ConsoleTest.java
+++ src/test/Takenoko/Util/ConsoleTest.java
@@ -10,71 +10,52 @@ import static org.junit.Assert.*;
 
 public class ConsoleTest {
 
-    /**
-     * Redirection du flux standard pour une éxécution silencieuse des tests
-     */
     @BeforeClass
     public static void setUp() {
         ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
     }
 
-    /**
-     * Console en mode par défaut -> pas d'affichage
-     */
     @Test
     public void init() {
-        assertTrue(Console.Log.init());
+        Console.Log.init();
+        
         assertFalse(Console.Log.print("test"));
         assertFalse(Console.Log.println("test"));
         assertFalse(Console.Log.debugPrint("test"));
         assertFalse(Console.Log.debugPrintln("test"));
     }
 
-    /**
-     * Console en mode non référencé -> erreur
-     */
-    @Test (expected = IllegalArgumentException.class)
-    public void initBadArgument() {
-        Console.Log.init("bambou");
-    }
-
-    /**
-     * Console en mode test -> pas d'afficage
-     */
     @Test
     public void initTest() {
-        assertTrue(Console.Log.init("test"));
+        Console.Log.init("test");
 
         assertFalse(Console.Log.print("test"));
         assertFalse(Console.Log.println("test"));
         assertFalse(Console.Log.debugPrint("test"));
         assertFalse(Console.Log.debugPrintln("test"));
+
     }
 
-    /**
-     * Console en mode release -> affichage des messages non debug
-     */
     @Test
     public void initRelease() {
-        assertTrue(Console.Log.init("release"));
+        Console.Log.init("release");
 
         assertTrue(Console.Log.print("test"));
         assertTrue(Console.Log.println("test"));
         assertFalse(Console.Log.debugPrint("test"));
         assertFalse(Console.Log.debugPrintln("test"));
+
     }
 
-    /**
-     * Console en mode debug -> affichage de tous les messages
-     */
     @Test
     public void initDebug() {
-        assertTrue(Console.Log.init("debug"));
+        Console.Log.init("debug");
 
         assertTrue(Console.Log.print("test"));
         assertTrue(Console.Log.println("test"));
         assertTrue(Console.Log.debugPrint("test"));
         assertTrue(Console.Log.debugPrintln("test"));
+
     }
 }
