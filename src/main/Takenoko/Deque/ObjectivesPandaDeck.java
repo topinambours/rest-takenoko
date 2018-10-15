015eaada5f7818ad210d7914f0bdeafe21862c79
Mahe Thomas <mt405333@etu.unice.fr>
2018-10-15 18:57:39 +0200

    security fix
    #89

--- src/main/Takenoko/Deque/ObjectivesPandaDeck.java
+++ src/main/Takenoko/Deque/ObjectivesPandaDeck.java
@@ -2,6 +2,7 @@ package Takenoko.Deque;
 
 import Takenoko.Objectives.ObjectiveCard;
 import Takenoko.Objectives.PandaObjectiveCard;
+import Takenoko.Util.Console;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
@@ -23,7 +24,7 @@ public class ObjectivesPandaDeck extends ObjectivesDeck {
         try {
             obj = mapper.readValue(file, mapper.getTypeFactory().constructCollectionType(List.class, PandaObjectiveCard.class));
         } catch (IOException e) {
-            e.printStackTrace();
+            Console.Log.debugPrint(e.getMessage());
         }
         return obj;
     }
