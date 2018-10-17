3c02d1a804efd6f1ff8b3780dccdcb816deb93cc
Fontany--Legall Brandon <brandon.fontany--legall@etu.unice.fr>
2018-10-17 10:56:29 +0200

    Définition d'une stratégie de choix d'action basique #94

--- src/main/Takenoko/Joueur/Strategie/StrategieAction/StrategieAction.java
+++ src/main/Takenoko/Joueur/Strategie/StrategieAction/StrategieAction.java
@@ -1,10 +1,30 @@
 package Takenoko.Joueur.Strategie.StrategieAction;
 
+import Takenoko.Game;
 import Takenoko.Plateau;
 
 public interface StrategieAction {
 
-    Action firstActionType(Plateau plat);
-    Action secondActionType(Plateau plat);
-    Action thirdActionType(Plateau plat);
+    Action firstActionType(Game game);
+    Action secondActionType(Game game);
+    Action thirdActionType(Game game);
+
+    default Action getLogicalNextAction(Action action){
+        switch (action){
+            case Gardener:
+                return Action.Plot;
+            case Panda:
+                return Action.Gardener;
+            case Irrig:
+                return Action.Gardener;
+            case Plot:
+                return Action.Card;
+            case Card:
+                return Action.Panda;
+            default:
+                return Action.Plot;
+
+        }
+
+    }
 }
