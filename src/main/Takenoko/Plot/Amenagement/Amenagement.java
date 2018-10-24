2f8633eaec866db199d607f45e41f27b57ed4df9
Fontany--Legall Brandon <brandon.fontany--legall@etu.unice.fr>
2018-10-24 10:03:42 +0200

    Ajout des aménagements #105

---
+++ src/main/Takenoko/Plot/Amenagement/Amenagement.java
@@ -0,0 +1,14 @@
+package Takenoko.Plot.Amenagement;
+
+/**
+ * Ajout des aménagements qui sont les suivants :
+ * <ul>
+ *     <li>ENCLOS</li>
+ *     <li>ENGRAIS</li>
+ *     <li>BASSIN</li>
+ * </ul>
+ * Il y a aussi NON lorsque la carte n'a pas d'amenagement
+ */
+public enum Amenagement {
+    NON,ENCLOS,ENGRAIS,BASSIN;
+}
