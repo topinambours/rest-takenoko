1c6737ed17cd910954d282bb9914c599883517d0
Mahe Thomas <mt405333@etu.unice.fr>
2018-10-30 14:01:26 +0100

    rename package phase 1

--- src/main/Takenoko/Util/Exceptions/NoActionSelectedException.java
+++
@@ -1,8 +0,0 @@
-package takenoko.util.exceptions;
-
-public class NoActionSelectedException extends Exception {
-
-    public NoActionSelectedException(){
-        super("Please select an action to do");
-    }
-}
