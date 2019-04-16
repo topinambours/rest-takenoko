ba2ba3823adb2304dc67ee89c39790d3c9bd4c51
Fontany--Legall Brandon <brandon.fontany--legall@etu.unice.fr>
2019-04-16 18:45:07 +0200

    stratégie de base, chi de tuile à poser #28

---
+++ client/src/main/java/core/strategie/RandomStrategie.java
@@ -0,0 +1,19 @@
+package core.strategie;
+
+import takenoko.tuile.CoordAxial;
+import takenoko.tuile.Tuile;
+
+import java.util.List;
+import java.util.Random;
+
+public class RandomStrategie implements Strategie {
+    public RandomStrategie() {
+    }
+
+    @Override
+    public Tuile selectTuile(List<Tuile> tuiles) {
+        Random rand = new Random();
+        return tuiles.get(rand.nextInt(tuiles.size()));
+    }
+
+}
---
+++ client/src/main/java/core/strategie/Strategie.java
@@ -0,0 +1,10 @@
+package core.strategie;
+
+import takenoko.tuile.CoordAxial;
+import takenoko.tuile.Tuile;
+
+import java.util.List;
+
+public interface Strategie {
+    Tuile selectTuile(List<Tuile> tuiles);
+}
