af4ae11643abd9e389bcba1226c6c1affc2c3a58
Mahe-Thomas <thomas.mahe@hamlab.fr>
2019-04-21 02:46:05 +0200

    Adding more stages to travis

---
+++ client/src/test/java/core/JoueurTest.java
@@ -0,0 +1,24 @@
+package core;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JoueurTest {
+
+    @Test
+    public void getHttpClient() {
+    }
+
+    @Test
+    public void setHttpClient() {
+    }
+
+    @Test
+    public void getId() {
+    }
+
+    @Test
+    public void turn() {
+    }
+}
