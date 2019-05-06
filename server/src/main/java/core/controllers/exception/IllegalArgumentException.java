013268bb1ad4d5224c19a2e6a93b93d3121cee5b
Fontany--Legall Brandon <brandon.fontany--legall@etu.unice.fr>
2019-05-06 18:15:31 +0200

    Update error http codes #118

--- server/src/main/java/core/controllers/exception/IllegalArgumentException.java
+++ server/src/main/java/core/controllers/exception/IllegalArgumentException.java
@@ -1,5 +1,9 @@
 package core.controllers.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
 public class IllegalArgumentException extends Exception {
     public IllegalArgumentException() {
         super("Illegal Argument");
