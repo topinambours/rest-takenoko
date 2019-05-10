79a8b38e5ea85758bfd93223a7dcc9bb27aa55e1
Fontany--Legall Brandon <brandon.fontany--legall@etu.unice.fr>
2019-05-10 14:38:28 +0200

    Authentification required exception #129

---
+++ server/src/main/java/core/controllers/exception/AuthentificationRequiredException.java
@@ -0,0 +1,11 @@
+package core.controllers.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
+public class AuthentificationRequiredException extends Exception {
+    public AuthentificationRequiredException() {
+        super("Error : Authentification Required");
+    }
+}
